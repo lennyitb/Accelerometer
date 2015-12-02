04ad20468bbf24887009e5cf0cae1a2ce5110c87
lennyitb <lennyphelan@gmail.com>
2015-12-02 10:01:57 -0500

    First commit!

---
+++ .build/uno/src/sketch.cpp
@@ -0,0 +1,90 @@
+#include <Arduino.h>
+
+void setup();
+void loop();
+#line 1 "src/sketch.ino"
+const int xpin = A5;
+const int ypin = A4;
+const int zpin = A3;
+
+/* Added by LHPIV as part of button interface */
+bool serialon = 0 ;
+
+/* number of milliseconds between readings */
+int sampleDelay = 50;
+
+void setup()
+{
+	/* initialize the serial communications: */
+	Serial.begin(9600);
+
+	/* Make sure the analog-to-digital converter takes its reference voltage from
+	 * the AREF pin */
+	analogReference(EXTERNAL);
+
+	pinMode(xpin, INPUT);
+	pinMode(ypin, INPUT);
+	pinMode(zpin, INPUT);
+	/* Added by LHPIV */
+	pinMode(8, INPUT_PULLUP);
+}
+
+void loop()
+{
+	int x = analogRead(xpin);
+
+	/* add a small delay between pin readings.  I read that you should
+	 * do this but haven't tested the importance */
+	delay(50);
+
+	int y = analogRead(ypin);
+
+	/* add a small delay between pin readings.  I read that you should
+	 * do this but haven't tested the importance */
+	delay(50);
+
+	int z = analogRead(zpin);
+
+	/* zero_G is the reading we expect from the sensor when it detects
+	 * no acceleration.  Subtract this value from the sensor reading to
+	 * get a shifted sensor reading. */
+	float zero_G = 512.0;
+	/* scale is the number of units we expect the sensor reading to
+	 * change when the acceleration along an axis changes by 1G.
+	 * Divide the shifted sensor reading by scale to get acceleration in Gs. */
+	float scale = 102.3;
+
+	/* Added by LHPIV as part of button interface */
+	int i;
+	for (i=1;i<=50;i++)
+	{
+		if (digitalRead(8)==LOW)
+		/* Pin 8 is configured as INPUT_PULLUP,
+		 *  Pressing the button pulls 8 from 5V to GND
+		 *  Therefore, logic is inverted. LOW now means
+		 *  the button IS pressed, and HIGH means it is
+		 *  not. -LHYIV*/
+		{
+			if (serialon==0)
+			{
+				serialon=1;
+			} else {
+				serialon=0;
+			}
+			delay(1000);
+		}
+		delay(1);
+	}
+	if (serialon==1)
+	{
+		Serial.print(((float)x - zero_G)/scale);
+		Serial.print("\t");
+		Serial.print(((float)y - zero_G)/scale);
+		Serial.print("\t");
+		Serial.print(((float)z - zero_G)/scale);
+		Serial.print("\n");
+		/* delay before next reading: */
+	}
+	/* delay(sampleDelay); replaced with a for loop that checks
+	 * if the button is pressed*/
+}
